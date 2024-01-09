fc497e9340fc82abd4eb5b91e412e8ce3628719b
ohseungmin <ohmin2886@gmail.com>
2024-01-09 21:47:46 +0900

    원본 틀

---
+++ surbur.js
@@ -0,0 +1,14 @@
+function surbur(callback) {
+  let request = {
+    insa : "Hello"
+  }
+  let response = {
+    insa : "bye"
+  };
+  return callback(request, response);
+}
+
+surbur(function(request, response) {
+  console.log(request.insa);
+  console.log(response.insa);
+})
